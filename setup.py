f403dfa8367e4ae2446607b0b9c1043a5f55e4fb
djungelorm <djungelorm@users.noreply.github.com>
2017-06-20 18:00:42 +0100

    Set classifiers to production/stable

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description = 'Tab views for Sphinx',
     install_requires = ['sphinx>=1.2'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
         'Environment :: Web Environment',
         'Framework :: Sphinx :: Extension',
