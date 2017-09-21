fa1271d4f1f5002804df74181fcab6bd354b1d50
djungelorm <djungelorm@users.noreply.github.com>
2017-09-21 22:19:48 +0100

    v1.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'sphinx-tabs',
-    version = '1.1.3',
+    version = '1.1.4',
     author = 'djungelorm',
     author_email = 'djungelorm@users.noreply.github.com',
     packages = ['sphinx_tabs'],
