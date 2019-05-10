e29869957397011f77b5b395730126713bc727fd
Honza Javorek <mail@honzajavorek.cz>
2019-05-10 22:37:48 +0200

    make sphinx-tabs compatible with sphinx 2.x
    Close https://github.com/djungelorm/sphinx-tabs/issues/39

--- sphinx_tabs/tabs.py
+++ sphinx_tabs/tabs.py
@@ -290,20 +290,20 @@ def update_context(app, pagename, templatename, context, doctree):
 def copy_assets(app, exception):
     """ Copy asset files to the output """
     if 'getLogger' in dir(logging):
-        log = logging.getLogger(__name__).info  # pylint: disable=no-member
+        log = logging.getLogger(__name__)  # pylint: disable=no-member
     else:
-        log = app.info
+        log = app
     builders = get_compatible_builders(app)
     if exception:
         return
     if app.builder.name not in builders:
         if not app.config['sphinx_tabs_nowarn']:
-            app.warn(
+            log.warn(
                 'Not copying tabs assets! Not compatible with %s builder' %
                 app.builder.name)
         return
 
-    log('Copying tabs assets')
+    log.info('Copying tabs assets')
 
     installdir = os.path.join(app.builder.outdir, '_static', 'sphinx_tabs')
 
