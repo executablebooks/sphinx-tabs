388502b3194dd00435fa5f93393bb2ab965272e7
foster999 <foster.dev999@gmail.com>
2020-09-04 21:18:44 +0100

    Fixed basic test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,15 @@
-import unittest
-import pkg_resources
-from sphinx_testing import with_app
-from .testcase import TestCase
+import pytest
 
-@with_app(buildername="html",srcdir=pkg_resources.resource_filename(__name__,"basic"))
-def test_build_html(self,app,status,warning):
-    app.builder.build_all()
-    actual = self.get_result(app, "index")
-    expected = self.get_expectation("basic", "index")
-    self.assertHasTabsAssets(actual)
-    self.assertXMLEqual(expected, actual)
-
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.sphinx(testroot='basic')
+def test_build_html(
+    app,
+    status,
+    warning,
+    check_build_success,
+    get_sphinx_app_output,
+    get_sphinx_app_doctree
+    ):
+    app.build()
+    check_build_success(status, warning)
+    get_sphinx_app_doctree(app, regress=True)
+    get_sphinx_app_output(app, regress=True)
