e87283f2d8d3a1cb857f52d4a07b012d62c321d0
David Foster <foster.dev999@gmail.com>
2022-06-25 23:35:52 +0100

    Drop sphinx 4 testing

--- tests/test_build.py
+++ tests/test_build.py
@@ -29,7 +29,7 @@ def test_conditional_assets(app, docname, check_asset_links):
 @pytest.mark.parametrize("docname", ["index", "no_tabs1", "no_tabs2"])
 @pytest.mark.sphinx(testroot="conditionalassets-policy")
 @pytest.mark.skipif(
-    sphinx.version_info[:2] < (4, 1), reason="Test uses Sphinx 4.1 config"
+    sphinx.version_info[:2] < (4, 1), reason="Test uses option that was introduced in Sphinx 4.1 "
 )
 def test_conditional_assets_html_assets_policy(
     app,
