1cd64e0d5b7eaf1a8f56ee8df2895eb4426e57c9
David Foster <foster.dev999@gmail.com>
2023-10-22 20:58:29 +0100

    Revert change in path comparison

--- sphinx_tabs/tabs.py
+++ sphinx_tabs/tabs.py
@@ -328,12 +328,12 @@ def update_context(app, pagename, templatename, context, doctree):
             context["css_files"][:] = context["css_files"]
             for path in paths:
                 if path.suffix == ".css" and path in context["css_files"]:
-                    context["css_files"].remove(path.filename)
+                    context["css_files"].remove(path.as_posix())
         if "script_files" in context:
             context["script_files"][:] = context["script_files"]
             for path in paths:
-                if path.suffix == ".js" and path.filename in context["script_files"]:
-                    context["script_files"].remove(path.filename)
+                if path.suffix == ".js" and path.as_posix() in context["script_files"]:
+                    context["script_files"].remove(path.as_posix())
 
 
 # pylint: enable=unused-argument
