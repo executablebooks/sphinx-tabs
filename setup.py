89104ddde06b3dc6873477eedf326a507c0fc516
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-12-21 13:34:08 -0600

    Support docutils 0.20.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     url="https://github.com/executablebooks/sphinx-tabs",
     license="MIT",
     python_requires="~=3.7",
-    install_requires=["sphinx", "pygments", "docutils~=0.18.0"],
+    install_requires=["sphinx", "pygments", "docutils>=0.18.0,<0.21.0"],
     extras_require={
         "testing": [
             "coverage",
@@ -51,6 +51,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python",
         "Topic :: Documentation :: Sphinx",
         "Topic :: Documentation",
